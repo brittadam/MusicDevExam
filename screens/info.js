cdc887106cf4b6d90d5db86b440637b64db3bc3f
brittadam <britt.adam@hotmail.com>
2023-01-01 17:51:44 +0100

    Eerste aanzet detailpagina

---
+++ screens/info.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import React, {useState, useEffect} from 'react';
+import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import Details from '../components/MusicDetails';
+
+// const Stack = createNativeStackNavigator();
+
+const info = ({navigation, route}) =>{
+
+    return (
+        <View>
+          <Details songTitle={route.params.itemTitle} 
+          singer={route.params.itemDescription}/>
+        </View>
+    )
+}
+
+export default info;
