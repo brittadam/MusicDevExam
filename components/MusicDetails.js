2fa38ea372d6fdddbab62dc0f068705bf0d2eee2
brittadam <britt.adam@hotmail.com>
2023-01-02 22:26:37 +0100

    Foto ook toegevoegd aan detailscherm en opmaak

--- components/MusicDetails.js
+++ components/MusicDetails.js
@@ -1,22 +1,48 @@
 
 
 import React, {useState, useEffect} from 'react';
-import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
 
 
 
 const Details = props =>{
 
     return (
-        <View>
-           <Text>
+        <View style={Styles.achtergrond}>
+           <Text style={Styles.title}>
             {props.songTitle} 
            </Text>
-           <Text>
+           <View style={Styles.foto}> 
+                <Image style={{width: 350, height: 250,}} source={{uri:`${props.photo}`}}></Image>
+           </View>
+           <Text style={Styles.singer}>
             {props.singer}
            </Text>
         </View>
     )
 }
+
+const Styles = StyleSheet.create({
+    foto:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    achtergrond:{
+        backgroundColor:"#565656",
+        paddingBottom: 350
+    },
+    title:{
+        color:"#90EE90",
+        textAlign: 'left',
+        fontSize: "30px", 
+        margin:20
+    },
+    singer:{
+        color:"#fff",
+        fontSize: "20px",
+        textAlign: 'left',
+        margin:20
+    }
+});
 
 export default Details;
