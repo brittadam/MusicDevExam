811881c2633262604ba25aa4b772d75fad4a7dd3
brittadam <britt.adam@hotmail.com>
2023-01-15 11:00:43 +0100

    Final changes overzichtscherm liedjes

--- screens/music.js
+++ screens/music.js
@@ -1,18 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList, Button} from 'react-native';
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Pressable, FlatList } from 'react-native';
+
+
 
 const Stack = createNativeStackNavigator();
 
-const music = ({navigation}) =>{
+const Music = ({ navigation }) => {
 
-    const [music, setMusic] = useState ([]);
+    const [music, setMusic] = useState([]);
 
     const getMusic = async () => {
         try {
-            const response = await fetch ("https://brittadam.be/wp-json/wp/v2/posts?categories=6", {
+            const response = await fetch("https://brittadam.be/wp-json/wp/v2/posts?categories=6", {
 
             })
             const json = await response.json();
@@ -33,133 +35,158 @@ const music = ({navigation}) =>{
         setCounter((currentCounter) => currentCounter + 1);
     };
 
-    const getMusicByTitleSearch = async (enteredText) => {//argument meegegeven door onChangeText
+    const getMusicByTitleSearch = async (enteredText) => {
         try {
-          if (enteredText.length > 0) {
-            const search = encodeURI("https://brittadam.be/wp-json/wp/v2/posts?categories=6&search=" + enteredText);
-            console.log(search);
-            const response = await fetch(search);
-            const json = await response.json();
-            console.log(json);
-            setMusic(json);
-          }
+            if (enteredText.length > 0) {
+                const search = encodeURI("https://brittadam.be/wp-json/wp/v2/posts?categories=6&search=" + enteredText);
+                console.log(search);
+                const response = await fetch(search);
+                const json = await response.json();
+                console.log(json);
+                setMusic(json);
+            }
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      }
+    }
 
     return (
-       
+
         <View style={Styles.algemeen}>
             <View style={Styles.addCounter}>
-                <Image source={require("../assets/picwish.png")} style={{margin: 10,width:50, height:50}}></Image>
+                <Image source={require("../assets/picwish.png")} style={{ margin: 10, width: 50, height: 50 }}></Image>
                 <Text style={Styles.counter}>{counter}</Text>
             </View>
             <TextInput
                 placeholder="Search movie"
                 style={Styles.input}
-                onChangeText={getMusicByTitleSearch}//geeft argument enteredText mee, denk aan de taskInputHandler uit de todo app.
+                onChangeText={getMusicByTitleSearch}
             />
 
-           <Button title="Follow artists" onPress={() => navigation.navigate("Follow")} />
+            <Pressable style={Styles.b} onPress={() => navigation.navigate("Follow")}>
+                <Text style={Styles.followbtn}>Follow artists</Text>
+            </Pressable>
 
-            <FlatList style={{height:610}} data={music} renderItem={({item}) => (
+            <FlatList style={{ height: 525 }} data={music} renderItem={({ item }) => (
                 <View style={Styles.achtergrond}>
                     <View style={Styles.container}>
                         <View>
-                        {item.yoast_head_json?.og_image !== undefined && <Image
-                        style={{ width: 300, height: 200, justifyContent: 'center',
-                        alignItems: 'center'}}
-                        source={{ uri: `${item.yoast_head_json.og_image[0].url}` }}
-                        />
-                        }
+                            {item.yoast_head_json?.og_image !== undefined && <Image
+                                style={{
+                                    width: 300, height: 200, justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                                source={{ uri: `${item.yoast_head_json.og_image[0].url}` }}
+                            />
+                            }
                         </View>
                         <Text style={Styles.song}>{item.title.rendered}</Text>
-                    
+
                     </View>
-                    <Pressable style={Styles.b} onPress ={()=> navigation.navigate ("info", {itemTitle: item.title.rendered, itemDescription: item.rttpg_excerpt, itemImage: item.yoast_head_json.og_image[0].url })}>
+                    <Pressable style={Styles.b} onPress={() => navigation.navigate("info", { itemTitle: item.title.rendered, itemDescription: item.rttpg_excerpt, itemImage: item.yoast_head_json.og_image[0].url })}>
                         <Text style={Styles.btn}>View song</Text>
                     </Pressable>
                     <Pressable style={Styles.AddSong} onPress={increase}>
-                    <Text style={Styles.add}> Add song</Text>
+                        <Text style={Styles.add}> Add song</Text>
                     </Pressable>
 
                 </View>
-            )}/>
+            )} />
         </View>
-        
+
     )
 }
 
 
 const Styles = StyleSheet.create({
-    algemeen:{
-        backgroundColor:"#565656"
+    algemeen: {
+        backgroundColor: "#232628"
     },
-    achtergrond:{
-        backgroundColor:"#000",
-        margin: 10, 
+    achtergrond: {
+        backgroundColor: "#000",
+        margin: 10,
         borderWidth: 5,
         borderColor: "#90EE90",
         borderRadius: 20,
     },
-    addCounter:{
-        flexDirection:'row',
+    addCounter: {
+        flexDirection: 'row',
         position: 'relative',
-        left:270   
+        left: 270
 
     },
-    counter:{
+    counter: {
         fontSize: 40,
-        marginTop:12
+        marginTop: 12,
+        color: "white"
     },
-    container:{
+    container: {
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 30, 
-        
+        padding: 30,
+
     },
-    song:{
-        color:"#fff",
+    song: {
+        color: "#fff",
         paddingTop: 20
     },
-    btn:{
-        backgroundColor:"#90EE90",
-        width: 100, 
-        padding: 5,
+    btn: {
+        borderColor: "#90EE90",
+        width: 120,
+        padding: 10,
         margin: 10,
         textAlign: 'center',
-        
+        borderWidth: 5,
+        borderRadius: 15,
+        color: "#90EE90",
+        fontWeight: 'bold'
+
     },
-    b:{
+    followbtn: {
+        borderColor: "#90EE90",
+        width: 180,
+        padding: 15,
+        margin: 10,
+        textAlign: 'center',
+        borderWidth: 5,
+        borderRadius: 15,
+        color: "#90EE90",
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    b: {
         justifyContent: 'center',
         alignItems: 'center',
     },
-    add:{
-        backgroundColor:"#90EE90",
-        width: 100, 
-        padding: 5,
+    add: {
+        borderColor: "#90EE90",
+        borderWidth: 5,
+        borderRadius: 15,
+        color: "#90EE90",
+        fontWeight: 'bold',
+        width: 120,
+        padding: 10,
         margin: 10,
         textAlign: 'center',
     },
-    AddSong:{
+    AddSong: {
         justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 20
     },
-    input:{
+    input: {
         backgroundColor: "white",
-        marginTop: 10, 
+        marginTop: 10,
         marginBottom: 10,
         marginLeft: 45,
         marginRight: 45,
-        padding: 15, 
-        color: "#90EE90", 
+        padding: 15,
+        color: "#90EE90",
         borderWidth: 5,
         borderColor: "#90EE90",
         borderRadius: 20,
     }
 })
-    
 
-export default music;
+
+export default Music;
