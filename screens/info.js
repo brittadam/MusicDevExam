935fb98d074bc8967261084e7aa11d6ce8341095
brittadam <britt.adam@hotmail.com>
2023-01-15 11:01:32 +0100

    Final changes infoscherm van liedje

--- screens/info.js
+++ screens/info.js
@@ -1,22 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import React, {useState, useEffect} from 'react';
-import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import { View } from 'react-native';
 import Details from '../components/MusicDetails';
 
-// const Stack = createNativeStackNavigator();
 
-const info = ({navigation, route}) =>{
+const Info = ({ route }) => {
 
-    return (
-        <View>
-          <Details songTitle={route.params.itemTitle} 
-          singer={route.params.itemDescription}
-          photo={route.params.itemImage}/>
-        </View>
-    )
+  return (
+    <View>
+      <Details songTitle={route.params.itemTitle}
+        singer={route.params.itemDescription}
+        photo={route.params.itemImage} />
+    </View>
+  )
 }
 
 
-export default info;
+export default Info;
