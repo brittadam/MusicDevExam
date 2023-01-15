4894aa48f3f7da930c828e327af7ac25062f2b37
brittadam <britt.adam@hotmail.com>
2023-01-15 11:02:35 +0100

    Final changes follow artist scherm

--- screens/follow.js
+++ screens/follow.js
@@ -1,83 +1,80 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Pressable, FlatList } from 'react-native';
 
 
 
-const follow = ({navigation, route}) =>{
-    
+const Follow = () => {
+
     const [enteredArtist, setEnteredArtist] = useState('');
     const [artists, setArtists] = useState([]);
-  
+
     const artistInputHandler = (enteredText) => {
-      setEnteredArtist(enteredText);
+        setEnteredArtist(enteredText);
     }
     const addArtistHandler = () => {
-      setArtists((currentArtists) => [...currentArtists, enteredArtist]);
-      console.log(artists);
+        setArtists((currentArtists) => [...currentArtists, enteredArtist]);
+        console.log(artists);
     }
 
-    return(
-        
-     <View style={Styles.achtergrond}>
-        <TextInput  style={Styles.input} onChangeText={artistInputHandler} placeholder="Search Artist"/>
-        
-        <Pressable style={Styles.b} onPress={addArtistHandler}> 
-            <Text style={Styles.btn}> Follow </Text>
-        </Pressable>
-        
-        <FlatList style={{height:620}} data={artists} renderItem={(itemData)=>(
-            <View style={Styles.bgfollow} > 
-                <Image source={require("../assets/picwish.png")} style={{margin: 10,width:20, height:20}}></Image>
-                <Text style={Styles.artistfollow}>
-                    {itemData.item}
-                </Text>
-            </View>
-        )}/>
-     </View>
-   
+    return (
+
+        <View style={Styles.achtergrond}>
+            <TextInput style={Styles.input} onChangeText={artistInputHandler} placeholder="Search Artist" />
+
+            <Pressable style={Styles.b} onPress={addArtistHandler}>
+                <Text style={Styles.btn}> Follow </Text>
+            </Pressable>
+
+            <FlatList style={{ height: 620 }} data={artists} renderItem={(itemData) => (
+                <View style={Styles.bgfollow} >
+                    <Image source={require("../assets/picwish.png")} style={{ margin: 10, width: 20, height: 20 }}></Image>
+                    <Text style={Styles.artistfollow}>
+                        {itemData.item}
+                    </Text>
+                </View>
+            )} />
+        </View>
+
     )
 }
 
- const Styles = StyleSheet.create({
-    achtergrond:{
-        backgroundColor:"#232628",
-        
+const Styles = StyleSheet.create({
+    achtergrond: {
+        backgroundColor: "#232628",
+
     },
-    input:{
+    input: {
         backgroundColor: "white",
-        marginTop: 50, 
+        marginTop: 50,
         marginBottom: 10,
         marginLeft: 45,
         marginRight: 45,
-        padding: 15, 
-        color: "#90EE90", 
+        padding: 15,
+        color: "#90EE90",
         borderWidth: 5,
         borderColor: "#90EE90",
         borderRadius: 20,
     },
-    btn:{
+    btn: {
         borderColor: "#90EE90",
-        marginTop: 10, 
+        marginTop: 10,
         marginBottom: 30,
-        padding: 15, 
+        padding: 15,
         textAlign: 'center',
-        borderWidth:5,
-        borderRadius:20,
-        color: "#90EE90", 
+        borderWidth: 5,
+        borderRadius: 20,
+        color: "#90EE90",
         fontWeight: 'bold'
     },
-    b:{
-      
-        alignItems: 'center',  
+    b: {
+
+        alignItems: 'center',
     },
-    artistfollow:{
-        color:"white"
-        
+    artistfollow: {
+        color: "white"
+
     },
-    bgfollow:{
+    bgfollow: {
         flexDirection: "row",
         alignItems: "center",
         marginTop: 20,
@@ -85,12 +82,12 @@ const follow = ({navigation, route}) =>{
         marginRight: 45,
         padding: 15,
         borderColor: "#90EE90",
-        borderWidth:5, 
+        borderWidth: 5,
         borderRadius: 20,
-        backgroundColor:"black",
+        backgroundColor: "black",
     }
 
 
 });
 
-export default follow;
+export default Follow;
